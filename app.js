6f499a60af6f9e26209e8c9c8b0c37ac3af50079
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2021-12-09 13:38:07 +0530

    Database Connection

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+dotenv.config({ path: "./config.env" });
+
+// Connecting Database
+const DB = process.env.DB_CONNECTION;
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the home route");
